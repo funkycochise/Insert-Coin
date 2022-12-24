6b2da828ba217d27f59f6fe02bf24ae9384b2e4a
funkycochise <funkycochise@free.fr>
2022-12-24 05:13:35 +0100

    console

---
+++ Scripts/#insertcoin/console/install.sh
@@ -0,0 +1,23 @@
+# redirect stdout/stderr to a file
+#exec >run.txt 2>&1
+
+exec 3>&1
+special_echo () {
+    echo "$@" >&3
+}
+exec &>/dev/null
+
+special_echo "Installing latest console build updater"
+cd /media/fat
+mkdir /media/fat/Scripts/temp
+cd /media/fat/Scripts/temp
+wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/update_console.sh
+mv /media/fat/Scripts/temp/update_console.sh /media/fat/Scripts/#insertcoin/update
+rm -r /media/fat/Scripts/temp >/dev/null
+sh /media/fat/Scripts/#insertcoin/update/update_main_mister.sh
+special_echo "Completed."
+
+
+
+
+
