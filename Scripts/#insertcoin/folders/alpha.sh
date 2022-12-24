7dc585e8086cbd0fc61f1bb970f37aac1c332c0a
funkycochise <funkycochise@free.fr>
2022-12-24 06:00:10 +0100

    alpha

--- Scripts/#insertcoin/folders/alpha.sh
+++ Scripts/#insertcoin/folders/alpha.sh
@@ -19,6 +19,9 @@ orientation=$2
 if [ -z "$orientation" ] || [ "$orientation" = "V" ];
 then
    addgame "Sky Adventure (World).mra" "_Sky Adventure"
+   addgame "Sky Soldiers (US).mra" "_Sky Soldiers"
+   addgame "Time Soldiers (US Rev 3).mra" "_Time Soldiers"
+
 fi
 if [ -z "$orientation" ] || [ "$orientation" = "H" ];
 then
