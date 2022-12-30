4a77ad9457fe46e2615fc45e43c07973e7176cd2
funkycochise <funkycochise@free.fr>
2022-12-30 08:20:44 +0100

    newest function revamp

--- Scripts/#insertcoin/folders/newest.sh
+++ Scripts/#insertcoin/folders/newest.sh
@@ -19,11 +19,19 @@ cd $OutputRoot
 rm -r "/media/fat/_Arcade/$OutputRoot/$MainDir"
 create "$MainDir"
 
+function add {
+
+if  test -f "$RegSourceRoot/$1" ; then
+  linkfolder "$2"
+fi
+
+}
+
 counter=0
 
 function linkfolder {
    counter=$((counter+1))
-   if [[ "$counter" -lt 17 ]]; then
+   if [[ "$counter" -lt 21 ]]; then
       #special_echo "$counter - $1"
       formated=$(printf "%02d" $counter)
       ln -s "$ALT/$1"  "$OutputRoot/$MainDir/_$formated$1"
@@ -31,160 +39,98 @@ function linkfolder {
 }
 
 #1228
-if  test -f "$RegSourceRoot/Tiger Heli Bootleg.mra" ; then
-  linkfolder "_Tiger Heli"
-fi
+add "Tiger Heli Bootleg.mra" "_Tiger Heli"
 
 #1227
-if  test -f "$RegSourceRoot/SlapFight Bootleg.mra" ; then
-  linkfolder "_SlapFight"
-fi
+add "SlapFight Bootleg.mra" "_SlapFight"
 
 #1225
-if  test -f "$RegSourceRoot/Pipi & Bibis - Whoopee!! (Z80 sound cpu, set 1).mra" ; then
-  linkfolder "_Pipi & Bibis"
-fi
+add "Pipi & Bibis - Whoopee!! (Z80 sound cpu, set 1).mra" "_Pipi & Bibis"
+
 #1223
-if  test -f "$RegSourceRoot/Chelnov - Atomic Runner (World).mra" ; then
-  linkfolder "_Chelnov"
-fi
-#if  test -f "$RegSourceRoot/Karnov (US, rev 6).mra" ; then
-#  linkfolder "_Karnov"
-#fi
-if  test -f "$RegSourceRoot/Sky Soldiers (US).mra" ; then
-  linkfolder "_Sky Soldiers"
-fi
-if  test -f "$RegSourceRoot/Time Soldiers (US Rev 3).mra" ; then
-  linkfolder "_Time Soldiers"
-fi
+add "Chelnov - Atomic Runner (World).mra" "_Chelnov"
+add "Karnov (US, rev 6).mra" "_Karnov"
+add "Sky Soldiers (US).mra" "_Sky Soldiers"
+add "Time Soldiers (US Rev 3).mra" "_Time Soldiers"
 
 #1216
-if  test -f "$RegSourceRoot/Kageki (World).mra" ; then
-  linkfolder "_Kageki"
-fi
+add "Kageki (World).mra" "_Kageki"
 
 #1209
-if  test -f "$RegSourceRoot/Insector X (World).mra" ; then
-  linkfolder "_Insector X"
-fi
+add "Insector X (World).mra" "_Insector X"
 
 #1201
-if  test -f "$RegSourceRoot/Peter Pack Rat.mra" ; then
-  linkfolder "_Peter Pack Rat"
-fi
+add "Peter Pack Rat.mra" "_Peter Pack Rat"
 
 #1124
-if  test -f "$RegSourceRoot/XSleenaBA.mra" ; then
-  linkfolder "_Xain'd Sleena"
-fi
+add "XSleenaBA.mra" "_Xain'd Sleena"
 
 #1119
-if  test -f "$RegSourceRoot/Crystal Castles.mra" ; then
-  linkfolder "_Crystal Castles"
-fi
+add "Crystal Castles.mra" "_Crystal Castles"
 
 #1118
-if  test -f "$RegSourceRoot/Turbo Out Run (Out Run upgrade) (FD1094 317-0118).mra" ; then
-  linkfolder "_Turbo Out Run"
-fi
+add "Turbo Out Run (Out Run upgrade) (FD1094 317-0118).mra" "_Turbo Out Run"
 
 #1118
-if  test -f "$RegSourceRoot/Gang Wars.mra" ; then
-  linkfolder "_Gang Wars"
-fi
+add "Gang Wars.mra" "_Gang Wars"
 
 #1118
-if  test -f "$RegSourceRoot/Sky Adventure (World).mra" ; then
-  linkfolder "_Sky Adventure"
-fi
+add "Sky Adventure (World).mra" "_Sky Adventure"
 
 #1117
-if  test -f "$RegSourceRoot/Express Raider.mra" ; then
-  linkfolder "_Express Raider"
-fi
+add "Express Raider.mra" "_Express Raider"
 
 #1114
-if  test -f "$RegSourceRoot/Congo Bongo.mra" ; then
-  linkfolder "_Congo Bongo"
-fi
+add "Congo Bongo.mra" "_Congo Bongo"
 
 #1113
-if  test -f "$RegSourceRoot/Bosconian - Star Destroyer (new version).mra"; then
-  linkfolder "_Bosconian"
-fi
+add "Bosconian - Star Destroyer (new version).mra" "_Bosconian"
 
 #1113
-if  test -f "$RegSourceRoot/Tropical Angel.mra"; then
-  linkfolder "_Tropical Angel"
-fi
+add "Tropical Angel.mra" "_Tropical Angel"
 
 #1111
-if  test -f "$RegSourceRoot/Exerion (Jaleco).mra"; then
-  linkfolder "_Exerion"
-fi
+add "Exerion (Jaleco).mra" "_Exerion"
 
 #1109
-if  test -f "$RegSourceRoot/SonSon.mra"; then
-  linkfolder "_SonSon"
-fi
+add "SonSon.mra" "_SonSon"
 
 #1105
-if  test -f "$RegSourceRoot/Out Run (sitdown-upright, Rev B).mra"; then
-  linkfolder "_Out Run"
-fi
+add "Out Run (sitdown-upright, Rev B).mra" "_Out Run"
 
 #1010
-if  test -f "$RegSourceRoot/Nova 2001.mra"; then
-  linkfolder "_Nova 2001"
-fi
+add "Nova 2001.mra" "_Nova 2001"
 
 #0810
-if  test -f "$RegSourceRoot/Raiders5.mra"; then
-  linkfolder "_Raiders5"
-fi
+add "Raiders5.mra" "_Raiders5"
 
 #0510
-if  test -f "$RegSourceRoot/BankPanic.mra"; then
-  linkfolder "_BankPanic"
-fi
+add "BankPanic.mra" "_BankPanic"
 
 #0110
-if  test -f "$RegSourceRoot/Super Hang-On (sitdown-upright) (unprotected).mra"; then
-  linkfolder "_Super Hang-On"
-fi
+add "Super Hang-On (sitdown-upright) (unprotected).mra" "_Super Hang-On"
 
 #2909
-if  test -f "$RegSourceRoot/P.O.W. - Prisoners of War (US Version 1).mra"; then
-  linkfolder "_P.O.W. - Prisoners of War"
-fi
+add "P.O.W. - Prisoners of War (US Version 1).mra" "_P.O.W. - Prisoners of War"
 
 #2909
-if  test -f "$RegSourceRoot/Ikari III - The Rescue (World Version 1, 8-Way Joystick).mra"; then
-  linkfolder "_Ikari III - The Rescue"
-fi
+add "Ikari III - The Rescue (World Version 1, 8-Way Joystick).mra" "_Ikari III - The Rescue"
 
 #2509
-if  test -f "$RegSourceRoot/Street Smart (US Version 2).mra"; then
-  linkfolder "_Street Smart"
-fi
+add "Street Smart (US Version 2).mra" "_Street Smart"
+
 #2509
-if  test -f "$RegSourceRoot/SAR - Search And Rescue (World).mra"; then
-  linkfolder "_S.A.R. - Search and Rescue"
-fi
+add "SAR - Search And Rescue (World).mra" "_S.A.R. - Search and Rescue"
 
 #1409
-if  test -f "$RegSourceRoot/Cosmo Police Galivan (12-26-1985).mra"; then
-  linkfolder "_Galivan"
-fi
-#1009
-if  test -f "$RegSourceRoot/Ufo Robo Dangar (4-07-1987).mra"; then
-  linkfolder "_Ufo Robo Dangar"
-fi
+add "Cosmo Police Galivan (12-26-1985).mra" "_Galivan"
 
 #1009
-if  test -f "$RegSourceRoot/Karate Champ (US).mra"; then
-  linkfolder "_Karate Champ"
-fi
+add "Ufo Robo Dangar (4-07-1987).mra" "_Ufo Robo Dangar"
+
+#1009
+add "Karate Champ (US).mra" "_Karate Champ"
+
 #1009
 linkfolder "_R-Type II"
 #0509
@@ -198,9 +144,8 @@ linkfolder "_Air Duel"
 #3008
 linkfolder "_X Multiply"
 #2708
-if  test -f "$RegSourceRoot/Prehistoric Isle in 1930 (World).mra"; then
-   linkfolder "_Prehistoric Isle in 1930"
-fi
+add "Prehistoric Isle in 1930 (World).mra" "_Prehistoric Isle in 1930"
+
 #2508
 linkfolder "_Mr. HELI no Daibouken"
 #2508
@@ -209,9 +154,8 @@ linkfolder "_Legend of Hero Tonma"
 linkfolder "_Dragon Breed"
 
 #2208
-if  test -f "$RegSourceRoot/chameleon.mra"; then
-  linkfolder "_Chameleon"
-fi
+add ""$RegSourceRoot/chameleon.mra" "_Chameleon"
+
 #1608
 linkfolder "_Gallop"
 #1608
@@ -221,10 +165,6 @@ linkfolder "_Ninja Spirit"
 #2407
 #linkfolder "_R-Type"
 #0207
-if  test -f "$RegSourceRoot/Psychic 5 (World).mra"; then
-  linkfolder "_Psychic 5"
-fi
-
-
+add "Psychic 5 (World).mra" "_Psychic 5"
 
 exit 0
