ef4039f9719c44f05432f9e6b942d2985976529b
funkycochise <funkycochise@free.fr>
2023-03-04 07:27:03 +0100

    name revamping 04/03/2023

--- Scripts/#insertcoin/folders/capcom.sh
+++ Scripts/#insertcoin/folders/capcom.sh
@@ -26,24 +26,23 @@ orientation=$2
 if [ -z "$orientation" ] || [ "$orientation" = "V" ];
 then
    #special_echo "\$orientation is empty or V"
-   addgame  "1942.mra" "_1942"
-   addgame  "1943 Kai Midway Kaisen.mra" "_1943"
-   addgame  "1943 Midway Kaisen.mra" "_1943"
-   addgame  "1943 The Battle of Midway Mark II.mra" "_1943"
-   addgame  "1943 The Battle of Midway.mra" "_1943"
+   addgame  "1942 (Revision B).mra" "_1942"
+   addgame  "1943 Kai Midway Kaisen (Japan).mra" "_1943"
+   addgame  "1943 The Battle of Midway Mark II (US).mra" "_1943"
+   addgame  "1943 The Battle of Midway (Euro).mra" "_1943"
    addgame  "Block Block (World 911219 Joystick).mra" "_Block Block"
-   addgame  "Commando.mra" "_Commando"
+   addgame  "Commando (World).mra" "_Commando"
    addgame  "Exed Exes.mra" "_Exed Exes"
-   addgame  "Gun Smoke.mra" "_Gun.Smoke"
+   addgame  "Gun.Smoke (World, 1985-11-15).mra" "_Gun.Smoke"
    addgame  "Legendary Wings.mra" "_Legendary Wings"
    addgame  "The Speed Rumbler (set 1).mra" "_The Speed Rumbler"
-   addgame  "Vulgus.mra" "_Vulgus"
+   addgame  "Vulgus (set 1).mra" "_Vulgus"
    addgame  "SonSon.mra" "_SonSon"
 fi
 if [ -z "$orientation" ] || [ "$orientation" = "H" ];
 then
    #special_echo "\$orientation is empty or H"
-   addgame  "Bionic Commando.mra" "_Bionic Commando"
+   addgame  "Bionic Commando (Euro).mra" "_Bionic Commando"
    addgame  "Black Tiger.mra" "_Black Tiger"
    addgame  "Capcom World (Japan).mra" "_Capcom World"
    addgame  "Dokaben (Japan).mra" "_Dokaben"
@@ -51,12 +50,12 @@ then
    addgame  "Ghosts'n Goblins (World Set 1).mra" "_Ghosts'n Goblins"
    addgame  "Pang (World).mra" "_Pang"
    addgame  "Pirate Ship Higemaru.mra" "_Pirate Ship Higemaru"
-   addgame  "Savage Bees.mra" "_Exed Exes"
+   #addgame  "Savage Bees.mra" "_Exed Exes"
    addgame  "SectionZ.mra" "_SectionZ"
    addgame  "Side Arms - Hyper Dyne (World, 861129).mra" "_Side Arms"
    addgame  "Street Fighter (US, set 1).mra" "_Street Fighter"
    addgame  "Super Pang (World 900914).mra" "_Super Pang"
-   addgame  "Tiger Road.mra" "_Tiger Road"
+   addgame  "Tiger Road (US).mra" "_Tiger Road"
    addgame  "Trojan (Romstar).mra" "_Trojan"
 fi
 
