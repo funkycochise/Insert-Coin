07b691a1bd0d35e21d1b814d8202d78d6a60e1a3
funkycochise <funkycochise@free.fr>
2023-08-18 14:26:37 +0200

    new update/clean console

--- Scripts/#insertcoin/update/clean_console.sh
+++ Scripts/#insertcoin/update/clean_console.sh
@@ -4,6 +4,22 @@
 launchdir=$1
 #special_echo "launchdir $launchdir"
 
+CONSOLE=/media/fat/_Console
+
+function CleanSNES {
+
+  #get last revision
+  for f in $(ls $CONSOLE/SNES*.rbf)
+  do
+    target=${f:20:${#f}}
+    #echo "source: $f"
+    #echo "target: $target"
+
+  done
+  #remove all previous version
+  find $CONSOLE -maxdepth 1 -type f -name "SNES*" ! -name "$target" -delete
+}
+
 echo "Cleaning console mgl"
 
 #gameboy color ? remove
@@ -21,5 +37,9 @@ if [ -f "/media/fat/_Console/GnW_20220606.rbf" ]
 then
    rm -r "/media/fat/_Console/GnW_20220606.rbf"
 fi
+
+#keep only latest SNES console core
+CleanSNES
+
 
 echo "Completed."
--- Scripts/#insertcoin/update/update_console.sh
+++ Scripts/#insertcoin/update/update_console.sh
@@ -19,69 +19,52 @@ neogeo=$6
 
 function PSX {
 
-dualsdram=$1
-wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/PSX.zip --quiet
-unzip -qq PSX.zip
-rm -r PSX.zip
-files=( "$path"/*.rbf )
+  dualsdram=$1
+  wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/PSX.zip --quiet
+  unzip -qq PSX.zip
+  rm -r PSX.zip
+  files=( "$path"/*.rbf )
+  #echo "${#files[@]}"
+  #for f in $files
+  #do
+  #  echo "${f}"
+  #done
 
-for f in $(ls ./*.rbf)
-#for f in $files
-do
-  #echo "$f"
-  #echo "${f:2:4}"
-  if [ "$dualsdram" == "1" ]; then
-    if [ "${f:2:4}" == "Dual" ]; then
-      #echo "Dual sdram"
-      target=${f:7:${#f}}
-      #echo "source: $f"
-      #echo "target: $target"
-      mv $f $CONSOLE/$target
-      touch $CONSOLE/PSX*.rbf >/dev/null
-      echo "PSX"
+  for f in $(ls ./*.rbf)
+  do
+    #echo "$f"
+    #echo "${f:2:4}"
+    if [ "$dualsdram" == "1" ]; then
+      if [ "${f:2:4}" == "Dual" ]; then
+        #echo "Dual sdram"
+        target=${f:7:${#f}}
+        #echo "source: $f"
+        #echo "target: $target"
+        mv $f $CONSOLE/$target
+        touch $CONSOLE/PSX*.rbf >/dev/null
+	#remove keep only latest
+        find $CONSOLE -maxdepth 1 -type f -name "PSX*" ! -name "$target" -delete 
+        echo "PSX"
+      fi
+    else
+      if [ ! "${f:2:4}" == "Dual" ]; then
+        #echo "Single sdram"
+        target=${f:2:${#f}}
+        #echo "source: $f"
+        #echo "target: $target"
+        mv $f $CONSOLE/$target
+        touch $CONSOLE/PSX*.rbf >/dev/null
+	#remove keep only latest
+        find $CONSOLE -maxdepth 1 -type f -name "PSX*" ! -name "$target" -delete
+        echo "PSX"
+      fi
     fi
-  else
-    if [ ! "${f:2:4}" == "Dual" ]; then
-      #echo "Single sdram"
-      target=${f:2:${#f}}
-      #echo "source: $f"
-      #echo "target: $target"
-      mv $f $CONSOLE/$target
-      touch $CONSOLE/PSX*.rbf >/dev/null
-      echo "PSX"
+    #clean any file left
+    if test -f "./$f"; then
+      rm -r ./$f
     fi
-  fi
-  #clean any file left
-  if test -f "./$f"; then
-    rm -r ./$f
-  fi
-done
+  done
 }
-
-function s32x {
-  wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/S32X.zip --quiet
-  if test -f "./S32X.zip"; then
-    unzip S32X.zip >/dev/null 
-    rm -r ./S32X.zip >/dev/null
-    rm -r $CONSOLE/S32X*.rbf >/dev/null
-    mv ./S32X*.rbf $CONSOLE
-    touch $CONSOLE/S32X*.rbf >/dev/null
-    echo "S32X"
-  fi
-}
-
-function sgb {
-  wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/SGB.zip --quiet
-  if test -f "./SGB.zip"; then
-    unzip SGB.zip >/dev/null 
-    rm -r ./SGB.zip >/dev/null
-    rm -r $CONSOLE/SGB*.rbf >/dev/null
-    mv ./SGB*.rbf $CONSOLE
-    touch $CONSOLE/SGB*.rbf >/dev/null
-    echo "SGB"
-  fi
-}
-
 
 function Saturn {
   dualsdram=$1
@@ -109,6 +92,8 @@ function Saturn {
         #echo "target: $target"
         mv $f $CONSOLE/$target
         touch $CONSOLE/Saturn*.rbf >/dev/null
+	#remove keep only latest
+        find $CONSOLE -maxdepth 1 -type f -name "Saturn*" ! -name "$target" -delete 
         echo "Saturn"
       fi
     else
@@ -119,6 +104,8 @@ function Saturn {
         #echo "target: $target"
         mv $f $CONSOLE/$target
         touch $CONSOLE/Saturn*.rbf >/dev/null
+	#remove keep only latest
+        find $CONSOLE -maxdepth 1 -type f -name "Saturn*" ! -name "$target" -delete 
         echo "Saturn"
       fi
     fi
@@ -130,32 +117,88 @@ function Saturn {
   done
 }
 
-function neogeo {
-  wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/NeoGeo.zip --quiet
-  if test -f "./NeoGeo.zip"; then
-    unzip NeoGeo.zip >/dev/null 
-    rm -r ./NeoGeo.zip >/dev/null
-    rm -r $CONSOLE/NeoGeo*.rbf >/dev/null
-    mv ./NeoGeo*.rbf $CONSOLE
-    touch $CONSOLE/NeoGeo*.rbf >/dev/null
-    echo "NeoGeo"
-  fi
+
+function S32X {
+  wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/S32X.zip --quiet
+  unzip -qq S32X.zip
+  rm -r S32X.zip
+
+  for f in $(ls ./*.rbf)
+  do
+    target=${f:2:${#f}}
+    #echo "source: $f"
+    #echo "target: $target"
+    mv $f $CONSOLE/$target --force
+    touch $CONSOLE/$target >/dev/null
+    #remove keep only latest
+    find $CONSOLE -maxdepth 1 -type f -name "S32X*" ! -name "$target" -delete
+    echo "S32X"
+    #clean any file left
+    if test -f "./$f"; then
+      rm -r ./$f
+    fi
+  done
 }
+
+function SGB {
+  wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/SGB.zip --quiet
+  unzip -qq SGB.zip
+  rm -r SGB.zip
+
+  for f in $(ls ./*.rbf)
+  do
+    target=${f:2:${#f}}
+    #echo "source: $f"
+    #echo "target: $target"
+    mv $f $CONSOLE/$target --force
+    touch $CONSOLE/$target >/dev/null
+    #remove keep only latest
+    find $CONSOLE -maxdepth 1 -type f -name "SGB*" ! -name "$target" -delete
+    echo "SGB"
+    #clean any file left
+    if test -f "./$f"; then
+      rm -r ./$f
+    fi
+  done
+}
+
+function NeoGeo {
+  wget https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/NeoGeo.zip --quiet
+  unzip -qq NeoGeo.zip
+  rm -r NeoGeo.zip
+
+  for f in $(ls ./*.rbf)
+  do
+    target=${f:2:${#f}}
+    #echo "source: $f"
+    #echo "target: $target"
+    mv $f $CONSOLE/$target --force
+    touch $CONSOLE/$target >/dev/null
+    #remove keep only latest
+    find $CONSOLE -maxdepth 1 -type f -name "NeoGeo*" ! -name "$target" -delete
+    echo "NeoGeo"
+    #clean any file left
+    if test -f "./$f"; then
+      rm -r ./$f
+    fi
+  done
+}
+
 
 if [ "$psx" == "1" ]; then
   PSX "$dualsdram"
 fi
 if [ "$s32x" == "1" ]; then
-  s32x
+  S32X
 fi
 if [ "$sgb" == "1" ]; then
-  sgb
+  SGB
 fi
 if [ "$saturn" == "1" ]; then
   Saturn "$dualsdram"
 fi
 if [ "$neogeo" == "1" ]; then
-  neogeo
+  NeoGeo
 fi
 
 
