2b7014a180ddd0105f6bf00e933ab3805576452f
funkycochise <funkycochise@free.fr>
2024-02-16 16:00:45 +0100

    vendetta

--- Scripts/#insertcoin/folders/newest.sh
+++ Scripts/#insertcoin/folders/newest.sh
@@ -33,6 +33,9 @@ function linkfolder {
    fi
 }
 
+#0216
+add "Vendetta (World, 4 Players, ver. T).mra" "_Vendetta"
+
 #0209
 add "Parodius Da! Shinwa kara Owarai e (World, set 1).mra" "_Parodius Da! Shinwa kara Owarai e"
 
