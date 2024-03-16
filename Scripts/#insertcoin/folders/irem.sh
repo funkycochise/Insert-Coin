539d738bc4f575c893a425d450da949790d310b1
funkycochise <funkycochise@free.fr>
2024-03-16 14:45:13 +0100

    fix error in "

--- Scripts/#insertcoin/folders/irem.sh
+++ Scripts/#insertcoin/folders/irem.sh
@@ -8,7 +8,7 @@ vertical=$3
 stg_horizontal=$4
 stg_vertical=$5
 
-add "$dir" "V" "Shot Rider (B-Board 89624B-1).mra" "_Shot Rider "" "ACT"
+add "$dir" "V" "Shot Rider (B-Board 89624B-1).mra" "_Shot Rider" "" "ACT"
 add "$dir" "V" "Traverse USA- Zippy Race (US).mra" "_Traverse USA" "" "ACT"
 add "$dir" "H" "Moon Patrol.mra" "_Moon Patrol" "" "ACT"
 add "$dir" "H" "Vigilante (World, Rev E).mra" "_Vigilante" "" "ACT"
