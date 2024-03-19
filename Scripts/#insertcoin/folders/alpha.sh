c7a7e5fc88c0d811b4da0b4808e38a4c5bb58672
funkycochise <funkycochise@free.fr>
2024-03-19 12:21:32 +0100

    type

--- Scripts/#insertcoin/folders/alpha.sh
+++ Scripts/#insertcoin/folders/alpha.sh
@@ -8,7 +8,7 @@ vertical=$3
 stg_horizontal=$4
 stg_vertical=$5
 
-add "$dir" "H" "Gang Wars.mra" "_Gang Wars"
+add "$dir" "H" "Gang Wars.mra" "_Gang Wars" "" "BEA"
 add "$dir" "V" "Sky Adventure (World).mra" "_Sky Adventure" "" "STG"
 add "$dir" "V" "Sky Soldiers (US).mra" "_Sky Soldiers" "" "STG"
 add "$dir" "V" "Time Soldiers (US Rev 3).mra" "_Time Soldiers" "" "STG"
