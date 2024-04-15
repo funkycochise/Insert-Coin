5f78ba957af79d3933a2ae5b1206a7a1510dc99e
funkycochise <funkycochise@free.fr>
2024-04-15 12:24:58 +0200

    fix armed police batrider

--- Scripts/#insertcoin/folders/raizing.sh
+++ Scripts/#insertcoin/folders/raizing.sh
@@ -8,7 +8,7 @@ vertical=$3
 stg_horizontal=$4
 stg_vertical=$5
 
-add "$dir" "V"  "Armed Police Batrider Special Ver. (USA) (Fri Feb 13 1998).mra" "_Batrider" "" "STG"
+add "$dir" "V"  "Armed Police Batrider Special Ver. (USA) (Fri Feb 13 1998).mra" "_Armed Police Batrider" "" "STG"
 add "$dir" "V"  "Battle Bakraid - Unlimited Version (USA) (Tue Jun 8 1999).mra" "_Battle Bakraid" "" "STG"
 add "$dir" "V"  "Battle Garegga (Europe - USA - Japan - Asia) (Sat Feb 3 1996).mra" "_Battle Garegga" "" "STG"
 add "$dir" "V"  "Kingdom Grandprix.mra" "_Kingdom Grandprix" "" "STG"
