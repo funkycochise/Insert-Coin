ec696e250b30de12630ece6e40a2690da1d05744
funkycochise <funkycochise@free.fr>
2024-05-09 19:48:05 +0200

    Add files via upload

---
+++ Scripts/#insertcoin/folders/segasys18.sh
@@ -0,0 +1,22 @@
+#!/bin/bash  
+source ./folders/functions.sh
+
+dir="_Sega-System-18"
+outdir=$1
+horizontal=$2
+vertical=$3
+stg_horizontal=$4
+stg_vertical=$5
+
+add "$dir" "H" "Alien Storm (World, 2 Players) (FD1094 317-0154).mra" "_Alien Storm" "" "BEA"
+add "$dir" "H" "Bloxeed (Japan) (FD1094 317-0139).mra" "_Bloxeed" "" "PUZ"
+add "$dir" "H" "Clutch Hitter (US) (FD1094 317-0176).mra" "_Clutch Hitter" 
+add "$dir" "H" "D. D. Crew (World, 3 Players) (FD1094 317-0190).mra" "_D. D. Crew" "" "BEA"
+add "$dir" "H" "Desert Breaker (World) (FD1094 317-0196).mra" "_Desert Breaker"
+add "$dir" "H" "Hammer Away (Japan, prototype).mra" "_Hammer Away"
+add "$dir" "H" "Laser Ghost (World) (FD1094 317-0166).mra" "_Laser Ghost"
+add "$dir" "H" "Michael Jackson's Moonwalker (World) (FD1094-8751 317-0159).mra" "_Michael Jackson's Moonwalker" "" "ACT"
+add "$dir" "H" "Pontoon (FD1094 317-0153).mra" "_Pontoon"
+add "$dir" "H" "Shadow Dancer (World).mra" "_Shadow Dancer" "" "ACT"
+add "$dir" "H" "Wally wo Sagase! (rev B, Japan, 2 players) (FD1094 317-0197B).mra" "_Wally wo Sagase!"
+dot
