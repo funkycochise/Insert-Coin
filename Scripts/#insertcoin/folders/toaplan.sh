f1d703e20fc11b4c7098ed5a704f1cc57cba2f63
funkycochise <funkycochise@free.fr>
2024-05-12 07:25:36 +0200

    tekipaki

--- Scripts/#insertcoin/folders/toaplan.sh
+++ Scripts/#insertcoin/folders/toaplan.sh
@@ -20,6 +20,6 @@ add "$dir" "H" "Hellfire (2P set).mra" "_Hellfire" "" "STG"
 add "$dir" "H" "Pipi & Bibis - Whoopee!! (Z80 sound cpu, set 1).mra" "_Pipi & Bibis" "" "ACT"
 add "$dir" "H" "Snow Bros. 2 - With New Elves - Otenki Paradise (Hanafram).mra" "_Snow Bros. 2"
 add "$dir" "H" "Zero Wing (2P set).mra" "_Zero Wing" "" "STG"
-add "$dir" "H" "Teki Paki.mra" "_Teki Paki"
+add "$dir" "H" "Teki Paki.mra" "_Teki Paki" "" "PUZ"
 dot
 
