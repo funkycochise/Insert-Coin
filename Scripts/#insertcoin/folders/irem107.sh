fa8da9bca1cc8151ab30a8a9cec86e1d6b0ebf3b
funkycochise <funkycochise@free.fr>
2024-05-14 11:55:01 +0200

    Update irem107.sh

--- Scripts/#insertcoin/folders/irem107.sh
+++ Scripts/#insertcoin/folders/irem107.sh
@@ -5,7 +5,7 @@ dir="_Irem M107"
 outdir=$1
 loadsetup
 
-add "$dir" "V" "Air Assault (World, M107 hardware).mra" "_Air Assault" "" "STG"
+add "$dir" "V" "Air Assault (World).mra" "_Air Assault" "" "STG"
 
 
 dot
