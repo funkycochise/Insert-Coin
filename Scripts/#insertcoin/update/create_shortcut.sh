3528a1e7765be10c65bca4b9cac485b4929b79ae
funkycochise <funkycochise@free.fr>
2024-10-08 10:38:50 +0200

    Add files via upload

---
+++ Scripts/#insertcoin/update/create_shortcut.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source /media/fat/Scripts/#insertcoin/folders/functions.sh
+
+loadsetup
+
+finalfolder="_Insert Coin"
+folder="/media/fat/$finalfolder"
+if [ -d "$folder" ] 
+then
+  #echo "Removing $folder"
+  rm -r "$folder"
+fi 
+
+if [ "$root_symlink" == "1" ]; then
+   folder="/media/fat/_Arcade/$finalfolder"
+   if [ -d "$folder" ] 
+   then
+      echo "Creating shortcut /media/fat/$finalfolder"
+      ln -s "$folder" "/media/fat/$finalfolder"
+   fi
+fi
