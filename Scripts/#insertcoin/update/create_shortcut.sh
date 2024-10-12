2762197f279bf22931086b80143546104afd943f
funkycochise <funkycochise@free.fr>
2024-10-12 07:27:20 +0200

    Update create_shortcut.sh

--- Scripts/#insertcoin/update/create_shortcut.sh
+++ Scripts/#insertcoin/update/create_shortcut.sh
@@ -11,16 +11,16 @@ then
   rm -r "$folder"
 fi 
 
-if [ "$root_symlink" == "1" ]; then
-   folder="/media/fat/_Arcade/$finalfolder"
-   if [ -d "$folder" ] 
-   then
-      echo "Creating shortcut /media/fat/$finalfolder"
-      ln -s "$folder" "/media/fat/$finalfolder"
-      if [ "$TERM" == "linux" ]; then
-         #GUI
-         echo -n -e "   "
-      fi
-      echo -e "${GREEN}${CHECK}${NC} Completed"
-   fi
-fi
+#if [ "$root_symlink" == "1" ]; then
+#   folder="/media/fat/_Arcade/$finalfolder"
+#   if [ -d "$folder" ] 
+#   then
+#      echo "Creating shortcut /media/fat/$finalfolder"
+#      ln -s "$folder" "/media/fat/$finalfolder"
+#      if [ "$TERM" == "linux" ]; then
+#         #GUI
+#         echo -n -e "   "
+#      fi
+#      echo -e "${GREEN}${CHECK}${NC} Completed"
+#   fi
+#fi
