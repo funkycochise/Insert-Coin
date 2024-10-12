dfa397b5d16de95b22c2cd25e9f9f8f68bca4f12
funkycochise <funkycochise@free.fr>
2024-10-12 07:03:11 +0200

    X-Men

--- Scripts/#insertcoin/folders/newest.sh
+++ Scripts/#insertcoin/folders/newest.sh
@@ -31,130 +31,97 @@ function linkfolder {
    fi
 }
 
+#1011
+   add "X-Men (4 Players ver EBA).mra" "_X-Men"
 #0920
    add "Avengers (US, revision C).mra" "_Avengers"
-
 #0830
    add "Circus Charlie (level select, set 1).mra" "_Circus Charlie"
-
 #0818
    add "Salamander (version D).mra" "_Salamander"
    add "Kyukyoku Tiger (Japan, 2P Co-op).mra" "_Twin Cobra"
-
 #0728
    add "Sunset Riders (4 Players ver EAC).mra" "_Sunset Riders"
-
 #0712
    add "Michael Jackson's Moonwalker (World) (FD1094-8751 317-0159).mra" "_Michael Jackson's Moonwalker"
-
 #0628
    add "Bakutotsu Kijuutei.mra" "_Bakutotsu Kijuutei"
    add "Rompers (Japan, new version (Rev B)).mra" "_Rompers"
-
 #0622
-add "Super Locomotive.mra" "_Super Locomotive"
-
+   add "Super Locomotive.mra" "_Super Locomotive"
 #0614
-add "Clutch Hitter (US) (FD1094 317-0176).mra" "_Clutch Hitter"
-add "Tank Force (US, 2 Players).mra" "_Tank Force"
-
+   add "Clutch Hitter (US) (FD1094 317-0176).mra" "_Clutch Hitter"
+   add "Tank Force (US, 2 Players).mra" "_Tank Force"
 #0531
-add "Alien Storm (World, 2 Players) (FD1094 317-0154).mra" "_Alien Storm"
-
+   add "Alien Storm (World, 2 Players) (FD1094 317-0154).mra" "_Alien Storm"
 #0525
-add "Shadow Dancer (World).mra" "_Shadow Dancer"
-add "Bloxeed (Japan) (FD1094 317-0139).mra" "_Bloxeed"
-add "Quester (Japan).mra" "_Quester"
-add "Chou Zetsurinjin Berabowman (Japan, Rev C).mra" "_Chou Zetsurinjin Berabowman"
-
+   add "Shadow Dancer (World).mra" "_Shadow Dancer"
+   add "Bloxeed (Japan) (FD1094 317-0139).mra" "_Bloxeed"
+   add "Quester (Japan).mra" "_Quester"
+   add "Chou Zetsurinjin Berabowman (Japan, Rev C).mra" "_Chou Zetsurinjin Berabowman"
 #0514
-add "Air Assault (World).mra" "_Air Assault"
-
+   add "Air Assault (World).mra" "_Air Assault"
 #0512
-add "Teki Paki.mra" "_Teki Paki"
-
+   add "Teki Paki.mra" "_Teki Paki"
 #0503
-add "Boxy Boy (World, SB2).mra" "_Boxy Boy"
-add "Marchen Maze (Japan).mra" "_Marchen Maze"
-add "Shadowland (YD3).mra" "_Shadowland"
-
+   add "Boxy Boy (World, SB2).mra" "_Boxy Boy"
+   add "Marchen Maze (Japan).mra" "_Marchen Maze"
+   add "Shadowland (YD3).mra" "_Shadowland"
 #0419
-add "Blast Off (Japan).mra" "_Blast Off"
-add "Dangerous Seed (Japan).mra" "_Dangerous Seed" 
-add "Dragon Spirit (new version (DS3)).mra" "_Dragon Spirit"
-add "Pistol Daimyo no Bouken (Japan).mra" "_Pistol Daimyo no Bouken" 
-
+   add "Blast Off (Japan).mra" "_Blast Off"
+   add "Dangerous Seed (Japan).mra" "_Dangerous Seed" 
+   add "Dragon Spirit (new version (DS3)).mra" "_Dragon Spirit"
+   add "Pistol Daimyo no Bouken (Japan).mra" "_Pistol Daimyo no Bouken" 
 #0406
-add "Pac-Mania.mra" "_Pac-Mania"
-
+   add "Pac-Mania.mra" "_Pac-Mania"
 #0329
-add "Galaga '88.mra" "_Galaga '88"
-add "Toki (World, set 1).mra" "_Toki"
-
+   add "Galaga '88.mra" "_Galaga '88"
+   add "Toki (World, set 1).mra" "_Toki"
 #0216
-add "Vendetta (World, 4 Players, ver. T).mra" "_Vendetta"
-
+   add "Vendetta (World, 4 Players, ver. T).mra" "_Vendetta"
 #0209
-add "Parodius Da! Shinwa kara Owarai e (World, set 1).mra" "_Parodius Da! Shinwa kara Owarai e"
-
+   add "Parodius Da! Shinwa kara Owarai e (World, set 1).mra" "_Parodius Da! Shinwa kara Owarai e"
 #0121
-add "Carnival.mra" "_Carnival"
-add "Pulsar.mra" "_Pulsar"
-
+   add "Carnival.mra" "_Carnival"
+   add "Pulsar.mra" "_Pulsar"
 #0107
-add "Jitsuryoku!! Pro Yakyuu (Japan).mra" "_Jitsuryoku!! Pro Yakyuu"
-add "Plus Alpha.mra" "_Plus Alpha"
-
+   add "Jitsuryoku!! Pro Yakyuu (Japan).mra" "_Jitsuryoku!! Pro Yakyuu"
+   add "Plus Alpha.mra" "_Plus Alpha"
 #0102
 add "Hachoo.mra" "_Hachoo"
 add "In Your Face (US, Prototype).mra" "_In Your Face"
 add "" "_E.D.F. - Earth Defense Force"
-
 #1219
 add "Polaris.mra" "_Polaris"
-
 #1208
 add "Renegade (US).mra" "_Renegade"
 add "Tokio - Scramble Formation (newer).mra" "_Tokio"
-
 #1124
 add "Thunder Cross (set 1).mra" "_Thunder Cross"
-
 #1117
 add "Gang Busters (set 1).mra" "_Gang Busters"
-
 #1031
 add  "Splatter House (World, new version (SH3)).mra" "_Splatter House"
-
 #0924
 add "Iga Ninjyutsuden (Japan).mra" "_Ninja Kazan"
-
 #0901
 add "The Final Round (version M).mra" "_The Final Round"
-
 #0824
 add "Punk Shot (US 4 Players).mra" "_Punk Shot"
 add "M.I.A. - Missing in Action (version T).mra" "_M.I.A. - Missing in Action"
-
 #0818
 add "Teenage Mutant Ninja Turtles (World 4 Players, version X).mra" "_Teenage Mutant Ninja Turtles"
-
 #0811
 add "The Simpsons (4 Players World, set 1).mra" "_The Simpsons"
-
 #0716
 add "Saint Dragon (Set 1).mra" "_Saint Dragon"
-
 #0709
 add "" "_Avenging Spirit"
-
 #0702
 add "P-47 - The Freedom Fighter (Japan).mra" "_P-47 - The Freedom Fighter"
 add "The Lord of King (Japan).mra" "_The Astyanax"
-
 #0630
 add "Crime Fighters (World 2 players).mra" "_Crime Fighters"
-
 #0617
 add  "Alpine Ski.mra" "_Alpine Ski"
 add  "Bio Attack.mra" "_Bio Attack"
@@ -163,7 +130,6 @@ add  "High Way Race.mra" "_High Way Race"
 add  "Space Cruiser.mra" "_Space Cruiser"
 add  "Water Ski.mra" "_Water Ski"
 add  "Wild Western.mra" "_Wild Western"
-
 add  "Jungle Hunt.mra" "_Jungle Hunt"
 #add  "Elevator Action.mra" "_Elevator Action"
 add  "Elevator Action Bootleg.mra" "_Elevator Action"
@@ -174,25 +140,19 @@ add  "Sea Fighter Poseidon.mra" "_Sea Fighter Poseidon"
 add  "Space Seeker.mra" "_Space Seeker"
 add  "The Tin Star.mra" "_The Tin Star"
 add  "Time Tunnel.mra" "_Time Tunnel"
-
 #0609
 add "Demons World - Horror Story (Set 3).mra" "_Demon's World"
 add "Super Contra (set 1).mra" "_Super Contra"
-
 #0602
 add "Rally Bike - Dash Yarou.mra" "_Rally Bike"
 add "Kick And Run.mra" "_Kick And Run"
-
 #0527
 add "Vimana (Japan).mra" "_Vimana"
 add "Same! Same! Same! (2P Set).mra" "_Fire Shark"
-
 #0520
 add "Soldam.mra" "_Soldam"
-
 #0513
 add "Rod-Land (World, set 1).mra" "_Rod-Land"
-
 #0512
 add "Aliens (World set 1).mra" "_Aliens"
 add "Arkanoid - Revenge of DOH (World).mra" "_Arkanoid II"
@@ -202,18 +162,14 @@ add "Extermination (World).mra" "_Extermination"
 add "Kageki (World).mra" "_Kageki"
 add "Insector X (World).mra" "_Insector X"
 add "#sh (World, new version) (P0-043A PCB).mra" "_The NewZealand Story"
-
 #0428
 #add "Arkanoid - Revenge of DOH (World).mra" "_Arkanoid II"
 #add "M.I.A. - Missing in Action (rev T).mra" "_M.I.A. - Missing in Action"
 #add "Teenage Mutant Ninja Turtles (World 4 Players, version X).mra" "_Teenage Mutant Ninja Turtles"
-
 #0416
 add "Battletoads.mgl" "_Battletoads"
-
 #0414
 add "Haunted Castle (version M).mra" "_Haunted Castle"
-
 #0331
 add "Paddle Mania.mra" "_Paddle Mania"
 add "The Next Space (Japan).mra" "_The Next Space"
