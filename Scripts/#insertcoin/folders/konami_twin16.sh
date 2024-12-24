a3f755e87ad1231f9fbba38893ed96e8e4aaa3a5
funkycochise <funkycochise@free.fr>
2024-12-24 07:49:46 +0100

    Add files via upload

--- Scripts/#insertcoin/folders/konami_twin16.sh
+++ Scripts/#insertcoin/folders/konami_twin16.sh
@@ -6,6 +6,10 @@ res=$(exist "The Final Round (version M).mra")
 if [ "$res" == "1" ]; then
    dir="_Konami Twin16"
    outdir=$1
+   add "$dir" "H" "Cue Brick (Japan).mra" "_Cue Brick" "" "PUZ"
+   add "$dir" "H" "Devil World.mra" "_Devil World" "" "RNG"
    add "$dir" "H" "The Final Round (version M).mra" "_The Final Round" "" "SPO"
+   add "$dir" "H" "Vulcan Venture (New).mra" "_Vulcan Venture" "" "STG"
+
    dot
 fi
