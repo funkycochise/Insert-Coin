1f43371266340eb7dc72821058a8d36f6a1a0597
funkycochise <funkycochise@free.fr>
2024-12-28 08:43:09 +0100

    Update konami_twin16.sh

--- Scripts/#insertcoin/folders/konami_twin16.sh
+++ Scripts/#insertcoin/folders/konami_twin16.sh
@@ -8,8 +8,8 @@ if [ "$res" == "1" ]; then
    outdir=$1
    add "$dir" "H" "Cue Brick (Japan).mra" "_Cue Brick" "" "PUZ"
    add "$dir" "H" "Devil World.mra" "_Devil World" "" "RNG"
+   add "$dir" "H"  "M.I.A. - Missing in Action (version T).mra" "_M.I.A. - Missing in Action" "" "RNG"
    add "$dir" "H" "The Final Round (version M).mra" "_The Final Round" "" "SPO"
    add "$dir" "H" "Vulcan Venture (New).mra" "_Vulcan Venture" "" "STG"
-
    dot
 fi
