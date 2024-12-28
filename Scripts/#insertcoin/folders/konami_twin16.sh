f93683fa711690d0bbccc7026caa6e2b26b9d6d2
funkycochise <funkycochise@free.fr>
2024-12-28 08:45:21 +0100

    Update konami_twin16.sh

--- Scripts/#insertcoin/folders/konami_twin16.sh
+++ Scripts/#insertcoin/folders/konami_twin16.sh
@@ -6,6 +6,7 @@ res=$(exist "The Final Round (version M).mra")
 if [ "$res" == "1" ]; then
    dir="_Konami Twin16"
    outdir=$1
+   #add_folder 
    add "$dir" "H" "Cue Brick (Japan).mra" "_Cue Brick" "" "PUZ"
    add "$dir" "H" "Devil World.mra" "_Devil World" "" "RNG"
    add "$dir" "H"  "M.I.A. - Missing in Action (version T).mra" "_M.I.A. - Missing in Action" "" "RNG"
