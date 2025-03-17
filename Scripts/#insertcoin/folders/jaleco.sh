feab7040d9ae16fe1b6b56c236d558217183be3a
funkycochise <funkycochise@free.fr>
2025-03-17 12:35:08 +0100

    Update jaleco.sh

--- Scripts/#insertcoin/folders/jaleco.sh
+++ Scripts/#insertcoin/folders/jaleco.sh
@@ -8,10 +8,11 @@ if [ "$res" == "1" ]; then
    outdir=$1
 
    add "$dir" "V" "Exerion (Taito).mra" "_Exerion" "" "STG"
+   add "$dir" "V" "Naughty Boy (Jaleco).mra" "_Naughty Boy" "" "ACT"
    add "$dir" "V" "Plus Alpha.mra" "_Plus Alpha"
    add "$dir" "V" "Psychic 5 (World).mra" "_Psychic 5" "" "ACT"
    add "$dir" "V" "Psychic 5 (Japan).mra" "_Psychic 5" "" "ACT"
-   add "$dir" "V" "Naughty Boy (Jaleco).mra" "_Naughty Boy" "" "ACT"
+
 
    rep "$dir" "H" "_Avenging Spirit"  "_Avenging Spirit" "" "ACT"
    add "$dir" "H" "chameleon.mra" "_Chameleon" "" "ACT"
