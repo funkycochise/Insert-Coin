11665af070528d898cabc377c2c7fd772d80f94d
funkycochise <funkycochise@free.fr>
2025-03-31 20:19:17 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/raizing.sh
+++ Scripts/#insertcoin/folders/raizing.sh
@@ -2,14 +2,19 @@
 source ./folders/functions.sh
 loadsetup
 
-res=$(exist "Armed Police Batrider Special Ver. (USA) (Fri Feb 13 1998).mra")
+res=$(exist "Armed Police Batrider (Japan, B Version) (Fri Feb 13 1998).mra")
 if [ "$res" == "1" ]; then
    dir="_Raizing-8ing"
    outdir=$1
-   add "$dir" "V"  "Armed Police Batrider Special Ver. (USA) (Fri Feb 13 1998).mra" "_Armed Police Batrider" "" "STG"
-   add "$dir" "V"  "Battle Bakraid - Unlimited Version (USA) (Tue Jun 8 1999).mra" "_Battle Bakraid" "" "STG"
+   add "$dir" "V"  "Armed Police Batrider (Japan, B Version) (Fri Feb 13 1998).mra" "_Armed Police Batrider" "" "STG"
+   add "$dir" "V"  "Battle Bakraid - Unlimited Version (Japan) (Tue Jun 8 1999).mra" "_Battle Bakraid" "" "STG"
    add "$dir" "V"  "Battle Garegga (Europe - USA - Japan - Asia) (Sat Feb 3 1996).mra" "_Battle Garegga" "" "STG"
-   add "$dir" "V"  "Kingdom Grandprix.mra" "_Kingdom Grandprix" "" "STG"
-   add "$dir" "V"  "Sorcer Striker.mra" "_Sorcer Striker" "" "STG"
+   add "$dir" "V"  "Shippu Mahou Daisakusen (Japan).mra" "_Kingdom Grandprix" "" "STG"
+   rep "$dir" "V" "_Kingdom Grandprix"  "_Kingdom Grandprix" "" "STG"
+   add "$dir" "V"  "Mahou Daisakusen (Japan).mra" "_Sorcer Striker" "" "STG"
+   rep "$dir" "V" "_Sorcer Striker"  "_Sorcer Striker" "" "STG"
+
    dot
+else
+   echo -e "NF raizing mras\r" >> /media/fat/Scripts/#insertcoin/out.txt
 fi
