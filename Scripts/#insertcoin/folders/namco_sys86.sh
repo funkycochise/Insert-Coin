bfc867bb78d9a3a8517ce414dc9aa6bd830397d5
funkycochise <funkycochise@free.fr>
2025-04-20 07:27:56 +0200

    rolling thunder

--- Scripts/#insertcoin/folders/namco_sys86.sh
+++ Scripts/#insertcoin/folders/namco_sys86.sh
@@ -2,12 +2,17 @@
 source ./folders/functions.sh
 loadsetup
 
-res=$(exist "Rolling Thunder.mra")
+res=$(exist "Rolling Thunder (rev 3).mra")
 if [ "$res" == "1" ]; then
    dir="_Namco_System86"
    outdir=$1
 
 
-   add "$dir" "H" "Rolling Thunder.mra" "_Rolling Thunder" "" "ACT"
+   add "$dir" "H" "Rolling Thunder (rev 3).mra" "_Rolling Thunder" "" "ACT"
+   add "$dir" "H" "Sky Kid Deluxe (set 1).mra" "_Sky Kid Deluxe" "" "ACT"
+   add "$dir" "H" "Genpei ToumaDen.mra" "_Genpei ToumaDen" "" "ACT"
+   add "$dir" "H" "Hopping Mappy.mra" "_Hopping Mappy" "" "ACT"
+   add "$dir" "H" "The Return of Ishtar.mra" "_The Return of Ishtar"
+   add "$dir" "H" "Wonder Momo.mra" "_Wonder Momo" "" "ACT"
    dot
 fi
