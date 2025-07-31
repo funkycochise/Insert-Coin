73c36176dcdbe717f2f599d20b9ad8db132f879c
funkycochise <funkycochise@free.fr>
2025-07-31 10:24:45 +0200

    local, remove echo

--- Scripts/#insertcoin/update/install_local.sh
+++ Scripts/#insertcoin/update/install_local.sh
@@ -12,28 +12,28 @@ echo "Installing local resources"
    then
       if test -f "$addon/uni-bios.rom"; then
          cp $addon/uni-bios.rom /media/fat/games/NEOGEO
-         echo "local Uni-bios deployed"
+         #echo "local Uni-bios deployed"
       fi
       if test -f "$addon/neogeo.zip"; then
          cp $addon/neogeo.zip /media/fat/games/mame
-         echo "local Uni-bios deployed in mame folder"
+         #echo "local Uni-bios deployed in mame folder"
       fi
       for dir in $addon/_ic/*; do
          f=$(basename -- "$file")
-          echo "$dir/$f"     
+          #echo "$dir/$f"     
           cp -r "$dir/$f" "$destination"    
       done
    fi
    if [ -d "$addon/_Arcade/" ];
    then
-      echo "copying mra"
+      #echo "copying mra"
       cp /media/fat/Scripts/#local/addon/_Arcade/*.mra /media/fat/_Arcade
-      ls /media/fat/Scripts/#local/addon/_Arcade/*.mra
-      echo "copying rbf"
+      #ls /media/fat/Scripts/#local/addon/_Arcade/*.mra
+      #echo "copying rbf"
       cp /media/fat/Scripts/#local/addon/_Arcade/cores/*.rbf /media/fat/_Arcade/cores
-      ls /media/fat/Scripts/#local/addon/_Arcade/cores/*.rbf
+      #ls /media/fat/Scripts/#local/addon/_Arcade/cores/*.rbf
       cp -r /media/fat/Scripts/#local/addon/_Arcade/_alternatives /media/fat/_Arcade/_alternatives
-      ls /media/fat/Scripts/#local/addon/_Arcade/_alternatives
+      #ls /media/fat/Scripts/#local/addon/_Arcade/_alternatives
    fi
 
 
