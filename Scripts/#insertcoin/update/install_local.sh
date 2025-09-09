a54323503d951a1398ab387173c652dd98fdc9a5
funkycochise <funkycochise@free.fr>
2025-09-09 12:45:31 +0200

    Add files via upload

--- Scripts/#insertcoin/update/install_local.sh
+++ Scripts/#insertcoin/update/install_local.sh
@@ -2,45 +2,40 @@
 source /media/fat/Scripts/#insertcoin/folders/functions.sh
 addon="/media/fat/Scripts/#local/addon"
 
-echo "Installing local resources"
+echo "Installing local resources in $targetfolder"
 
 targetfolder=$1
 
-#echo "targetfolder : $targetfolder"
+function processlocal {
+   #echo "processlocal"
+   #echo "$1"
+   echo "$2"
+   folder="$1/$2"
+   if [ -d "$folder" ]; then
+      #echo "removing $folder"
+      rm -r "$folder"
+   fi
+   mkdir "$folder"
+   #echo "/media/fat/Scripts/#local/addon/_ic/$2/*.* $folder" 
+   cp $addon/_ic/$2/*.* $folder
+}
 
 if [ -d "$addon/_ic/" ];
 then
-  if test -f "$addon/uni-bios.rom"; then
+   if test -f "$addon/uni-bios.rom"; then
      cp $addon/uni-bios.rom /media/fat/games/NEOGEO
      #echo "local Uni-bios deployed"
-  fi
-  if test -f "$addon/neogeo.zip"; then
+   fi
+   if test -f "$addon/neogeo.zip"; then
      cp $addon/neogeo.zip /media/fat/games/mame
      #echo "local Uni-bios deployed in mame folder"
-  fi
-#if test -d "$addon/_ic/_#Saturn"; then
-#   echo  "$addon/_ic/_#Saturn to $targetfolder"
-#   cp -r "$addon/_ic/_#Saturn" "$targetfolder"
-#fi
-#if test -d "$addon/_ic/_#Pce"; then
-#   echo "$addon/_ic/_#Pce to $targetfolder"
-#   cp -r "$addon/_ic/_#Pce" "$targetfolder"
-#fi
-if [ ! -d "$targetfolder" ];then
-   mkdir $targetfolder
-fi
-for dir in $addon/_ic/*; do
-   f=$(basename -- "$dir")
-    echo "$f"
-    mkdir $targetfolder/$f
-    cp -r "$addon/_ic/$f/" "$targetfolder/$f" 
-done
-
-#for f in $(find  /media/fat/Scripts/#local/addon/_ic/ -mindepth 1 -maxdepth 1 -type d)
-#do
-#   echo "$f"
-#   cp -r $f $targetfolder
-#done
+   fi
+   if test -d "$addon/_ic/_#Saturn"; then
+      processlocal $targetfolder "_#Saturn"
+   fi
+   if test -d "$addon/_ic/_#Pce"; then
+      processlocal $targetfolder "_#Pce"
+   fi
 fi
    if [ -d "$addon/_Arcade/" ];then
 
