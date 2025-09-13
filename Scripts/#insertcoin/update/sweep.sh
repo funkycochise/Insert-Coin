1da016fca2e485054b9e800553b8d05be75f9388
funkycochise <funkycochise@free.fr>
2025-09-13 06:13:15 +0200

    Add files via upload

--- Scripts/#insertcoin/update/sweep.sh
+++ Scripts/#insertcoin/update/sweep.sh
@@ -1,21 +1,37 @@
 
 folder="/media/fat/_Arcade/_#Insert-Coin"
 if [ -d "$folder" ];then
+if [ "$TERM" == "linux" ]; then
+   #GUI
+   echo -n -e "   "
+fi
 echo "Removing $folder"
 rm -r "$folder"
 fi
 folder="/media/fat/_Arcade/_Insert-Coin"
 if [ -d "$folder" ];then
+if [ "$TERM" == "linux" ]; then
+   #GUI
+   echo -n -e "   "
+fi
 echo "Removing $folder"
 rm -r "$folder"
 fi
 folder="/media/fat/_Insert Coin"
 if [ -d "$folder" ];then
+if [ "$TERM" == "linux" ]; then
+   #GUI
+   echo -n -e "   "
+fi
 echo "Removing $folder"
 rm -r "$folder"
 fi
 folder="/media/fat/_#Insert-Coin"
 if [ -d "$folder" ];then
+if [ "$TERM" == "linux" ]; then
+   #GUI
+   echo -n -e "   "
+fi
 echo "Removing $folder"
 rm -r "$folder"
 fi
