bf5070b13899bc931e664d55b6d3b5b6a38d2b93
funkycochise <funkycochise@free.fr>
2025-09-15 07:54:24 +0200

    Add files via upload

--- Scripts/#insertcoin/update/sweep.sh
+++ Scripts/#insertcoin/update/sweep.sh
@@ -1,37 +1,14 @@
 
-folder="/media/fat/_Arcade/_#Insert-Coin"
-if [ -d "$folder" ];then
-if [ "$TERM" == "linux" ]; then
-   #GUI
-   echo -n -e "   "
-fi
-echo "Removing $folder"
-rm -r "$folder"
-fi
-folder="/media/fat/_Arcade/_Insert-Coin"
-if [ -d "$folder" ];then
-if [ "$TERM" == "linux" ]; then
-   #GUI
-   echo -n -e "   "
-fi
-echo "Removing $folder"
-rm -r "$folder"
-fi
-folder="/media/fat/_Insert Coin"
-if [ -d "$folder" ];then
-if [ "$TERM" == "linux" ]; then
-   #GUI
-   echo -n -e "   "
-fi
-echo "Removing $folder"
-rm -r "$folder"
-fi
-folder="/media/fat/_#Insert-Coin"
-if [ -d "$folder" ];then
-if [ "$TERM" == "linux" ]; then
-   #GUI
-   echo -n -e "   "
-fi
-echo "Removing $folder"
-rm -r "$folder"
-fi
+function removedir {
+
+   if [ -d "$1" ];then
+      echo "removing $1"
+      rm -r "$1"
+   fi
+}
+
+removedir "/media/fat/_Insert Coin"
+removedir "/media/fat/_#Insert-Coin"
+removedir "/media/fat/_Arcade/_Insert Coin"
+removedir "/media/fat/_Arcade/_#Insert-Coin"
+removedir "/media/fat/_Arcade/_#Insert_Coin"
