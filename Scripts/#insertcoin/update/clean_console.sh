23e842d6ee6b1c5729a19f1c23e43905612c10e9
funkycochise <funkycochise@free.fr>
2025-10-30 18:27:46 +0100

    Add files via upload

--- Scripts/#insertcoin/update/clean_console.sh
+++ Scripts/#insertcoin/update/clean_console.sh
@@ -5,19 +5,6 @@ loadsetup
 
 CONSOLE=/media/fat/_Console
 
-function CleanSNES {
-
-  #get last revision
-  for f in $(ls $CONSOLE/SNES*.rbf)
-  do
-    target=${f:20:${#f}}
-    #echo "source: $f"
-    #echo "target: $target"
-
-  done
-  #remove all previous version
-  find $CONSOLE -maxdepth 1 -type f -name "SNES*" ! -name "$target" -delete
-}
 
 if [ "$TERM" == "linux" ]; then
    #GUI
@@ -46,9 +33,6 @@ if [ -f "/media/fat/_Console/NeoGeo_20250206.rbf" ];then
    fi
 fi
 
-#keep only latest SNES console core
-#disabled for instance
-#CleanSNES
 
 if [ "$TERM" == "linux" ]; then
    #GUI
