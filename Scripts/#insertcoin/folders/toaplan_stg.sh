66d9f33463912ab782760a39084b550064f658b8
funkycochise <funkycochise@free.fr>
2025-12-11 08:38:00 +0100

    Update toaplan_stg.sh

--- Scripts/#insertcoin/folders/toaplan_stg.sh
+++ Scripts/#insertcoin/folders/toaplan_stg.sh
@@ -3,13 +3,13 @@ source ./folders/functions.sh
 loadsetup
 
 if [ -f "$names" ]; then
-   source <(grep toaplan $names)
+   source <(grep toaplan_stg $names)
 else
-   toaplan="_Toaplan"
+   toaplan_stg="_Toaplan_STG"
 fi
-dir=$toaplan
+dir=$toaplan_stg
 
-resh=$(exist "Demon's World - Horror Story (Set 3).mra")
+resh=$(exist "Kyukyoku Tiger (Japan, 2P Co-op).mra")
 resv=$(exist "Truxton - Tatsujin.mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    outdir=$1
@@ -24,12 +24,5 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "Truxton - Tatsujin.mra" "_Truxton" "" "STG"
    add "$dir" "V" "Truxton II - Tatsujin Oh.mra" "_Truxton II" "" "STG"
    add "$dir" "V" "Vimana (Japan).mra" "_Vimana" "" "STG"
-
-   add "$dir" "H" "Demon's World - Horror Story (Set 3).mra" "_Demon's World" "" "RNG"
-   add "$dir" "H" "Hellfire (2P set).mra" "_Hellfire" "" "STG"
-   add "$dir" "H" "Pipi & Bibis - Whoopee!! (Z80 sound cpu, set 1).mra" "_Pipi & Bibis" "" "ACT"
-   add "$dir" "H" "Snow Bros. 2 - With New Elves - Otenki Paradise (Hanafram).mra" "_Snow Bros. 2"
-   add "$dir" "H" "Zero Wing (2P set).mra" "_Zero Wing" "" "STG"
-   add "$dir" "H" "Teki Paki.mra" "_Teki Paki" "" "PUZ"
    dot
 fi
