459a0eb47b124caa0ae5f6a8de2e89d70ec8e8f3
funkycochise <funkycochise@free.fr>
2025-12-11 19:40:19 +0100

    Add files via upload

--- Scripts/#insertcoin/folders/toaplan.sh
+++ Scripts/#insertcoin/folders/toaplan.sh
@@ -18,7 +18,6 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "H" "Pipi & Bibis - Whoopee!! (Z80 sound cpu, set 1).mra" "_Pipi & Bibis" "" "ACT"
    add "$dir" "H" "Snow Bros. 2 - With New Elves - Otenki Paradise (Hanafram).mra" "_Snow Bros. 2"
    add "$dir" "H" "Teki Paki.mra" "_Teki Paki" "" "PUZ"
-   add "$dir" "H" "Zero Wing (2P set).mra" "_Zero Wing" "" "STG"
    add "$dir" "V" "Performan (Japan).mra" "_Performan"
    add "$dir" "V" "Rally Bike - Dash Yarou.mra" "_Rally Bike"
 dot
