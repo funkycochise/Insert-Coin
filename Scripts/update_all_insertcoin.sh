708de340d46c951661a3a74c9df2e43a2e549ad6
funkycochise <funkycochise@free.fr>
2026-01-17 06:30:36 +0100

    Add files via upload

--- Scripts/update_all_insertcoin.sh
+++ Scripts/update_all_insertcoin.sh
@@ -4,10 +4,11 @@
 cd /media/fat/Scripts/#insertcoin
 python setup.py
 RET=$?
+
+
 if [ $RET -eq 0 ]; then
     #echo "Run choisi"
     ./run.sh | tee output.log
-    # Lancer la commande Run correspondante
 #elif [ $RET -eq 1 ]; then
 #    #echo "ESC ou Exit choisi"
 #    # Action en cas d'interruption
@@ -15,4 +16,4 @@ if [ $RET -eq 0 ]; then
 #    echo "Autre code retour : $RET"
 fi
 
-#./run.sh | tee output.log
+#echo "code retour : $RET"
