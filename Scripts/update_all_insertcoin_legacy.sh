0f46bf52c6eef77b3993a2d5b0a18dcc62365e14
funkycochise <funkycochise@free.fr>
2026-01-18 16:43:42 +0100

    Add files via upload

--- Scripts/update_all_insertcoin_legacy.sh
+++ Scripts/update_all_insertcoin_legacy.sh
@@ -3,16 +3,13 @@
 function dl {
 name=$1
 file=$2
-dl="0"
 
-
-if [ ! -f "$file" ] then
-   dl="1"
-fi
-if [ "$dl" == "1" ]; then
+if [ ! -f "$file" ]; then
   echo "Getting latest default Insert-Coin $name"
   curl "https://raw.githubusercontent.com/funkycochise/Insert-Coin_Res/main/$name" --insecure -o "$2" 
 fi
+}
+
 dl "setup.ini" "/media/fat/Scripts/#insertcoin/setup.ini"
 dl "names.ini" "/media/fat/Scripts/#insertcoin/names.ini"
 
