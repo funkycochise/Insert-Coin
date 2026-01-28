82d1918e7ad063c153179078749c322ecb66581e
funkycochise <funkycochise@free.fr>
2026-01-28 08:49:11 +0100

    Update toaplan_stg.sh

--- Scripts/#insertcoin/folders/toaplan_stg.sh
+++ Scripts/#insertcoin/folders/toaplan_stg.sh
@@ -20,9 +20,11 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "Out Zone.mra" "_Out Zone" "" "RNG"
    add "$dir" "V" "Same! Same! Same! (2P Set).mra" "_Fire Shark" "" "STG"
    add "$dir" "V" "SlapFight Bootleg.mra" "_SlapFight" "" "STG"
+   add "$dir" "V" "Tiger Heli Bootleg.mra" "_Tiger Heli" "" "STG"
    add "$dir" "V" "Truxton - Tatsujin.mra" "_Truxton" "" "STG"
    add "$dir" "V" "Truxton II - Tatsujin Oh.mra" "_Truxton II" "" "STG"
    add "$dir" "V" "Vimana (Japan).mra" "_Vimana" "" "STG"
    add "$dir" "H" "Zero Wing (2P set).mra" "_Zero Wing" "" "STG"
+   
 dot
 fi
