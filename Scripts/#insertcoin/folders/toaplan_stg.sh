251f6f8bbfa4a00c6f14ae2a5d1c3c7e7e11f966
funkycochise <funkycochise@free.fr>
2026-02-09 20:24:28 +0100

    Add files via upload

--- Scripts/#insertcoin/folders/toaplan_stg.sh
+++ Scripts/#insertcoin/folders/toaplan_stg.sh
@@ -2,11 +2,11 @@
 source ./folders/functions.sh
 loadsetup
 
-if [ -f "$names" ]; then
-   source <(grep toaplan_stg $names)
-else
+#if [ -f "$names" ]; then
+#   source <(grep toaplan_stg $names)
+#else
    toaplan_stg="_Toaplan_STG"
-fi
+#fi
 dir=$toaplan_stg
 
 resh=$(exist "Kyukyoku Tiger (Japan, 2P Co-op).mra")
@@ -16,16 +16,13 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
 
    add "$dir" "H" "Hellfire (2P set).mra" "_Hellfire" "" "STG"
    add "$dir" "V" "Alcon Bootleg.mra" "_SlapFight" "" "STG"
-   add "$dir" "V" "Hishou Zame (Japan).mra" "_Flying Shark" "" "STG"
    add "$dir" "V" "Kyukyoku Tiger (Japan, 2P Co-op).mra" "_Twin Cobra" "" "STG"
    add "$dir" "V" "Out Zone.mra" "_Out Zone" "" "RNG"
    add "$dir" "V" "Same! Same! Same! (2P Set).mra" "_Fire Shark" "" "STG"
    add "$dir" "V" "SlapFight Bootleg.mra" "_SlapFight" "" "STG"
-   add "$dir" "V" "Tiger Heli Bootleg.mra" "_Tiger Heli" "" "STG"
    add "$dir" "V" "Truxton - Tatsujin.mra" "_Truxton" "" "STG"
    add "$dir" "V" "Truxton II - Tatsujin Oh.mra" "_Truxton II" "" "STG"
    add "$dir" "V" "Vimana (Japan).mra" "_Vimana" "" "STG"
    add "$dir" "H" "Zero Wing (2P set).mra" "_Zero Wing" "" "STG"
-   
 dot
 fi
