94db057493c060e2860ad59d562b3083d5d45731
funkycochise <funkycochise@free.fr>
2026-03-07 09:18:34 +0100

    Add files via upload

--- Scripts/#insertcoin/folders/toaplan_stg.sh
+++ Scripts/#insertcoin/folders/toaplan_stg.sh
@@ -32,6 +32,7 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "Kyukyoku Tiger (Japan, 2P Co-op).mra" "_Twin Cobra" "" "STG"
    add "$dir" "V" "Out Zone.mra" "_Out Zone" "" "RNG"
    add "$dir" "V" "Same! Same! Same! (2P Set).mra" "_Fire Shark" "" "STG"
+   add "$dir" "V" "Hishou Zame (Japan).mra" "_Flying Shark" "" "STG"
    add "$dir" "V" "SlapFight Bootleg.mra" "_SlapFight" "" "STG"
    add "$dir" "V" "Truxton - Tatsujin.mra" "_Truxton" "" "STG"
    add "$dir" "V" "Truxton II - Tatsujin Oh.mra" "_Truxton II" "" "STG"
