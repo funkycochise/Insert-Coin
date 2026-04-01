02c7b2461e5da751643ab44303ad895e38da03d2
funkycochise <funkycochise@free.fr>
2026-04-01 10:26:16 +0200

    mat mania / mania challenge

--- Scripts/#insertcoin/folders/technos.sh
+++ Scripts/#insertcoin/folders/technos.sh
@@ -18,7 +18,9 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
 
    add "$dir" "H" "Double Dragon (World set 1).mra" "_Double Dragon" "" "BEA"
    add "$dir" "H" "Double Dragon II The Revenge (World).mra" "_Double Dragon II The Revenge" "" "BEA"
-   add "$dir" "H" "V'Ball (US).mra" "_V'Ball" "" "SPO"
+   add "$dir" "H" "Mat Mania.mra" "_Mat Mania" "" "SPO"
+   add "$dir" "H" "Mania Challenge (Set 1).mra" "_Mania Challenge" "" "SPO"
    add "$dir" "H" "Renegade (US).mra" "_Renegade" "" "BEA"
+   add "$dir" "H" "V'Ball (US).mra" "_V'Ball" "" "SPO"
    dot
 fi
