e5def0de475499ee9fa8a7b88f9624570f6f390e
funkycochise <funkycochise@free.fr>
2026-04-26 07:52:33 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/Tehkan-Tecmo.sh
+++ Scripts/#insertcoin/folders/Tehkan-Tecmo.sh
@@ -18,7 +18,7 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "Gemini Wing.mra" "_Gemini Wing" "" "STG"
    #add "$dir" "V" "Pleiads (Centuri).mra" "_Pleiads" "" "STG"
    add "$dir" "V" "Pleiads (Tehkan).mra" "_Pleiads" "" "STG"
-   add "$dir" "V" "StarForce.mra" "_StarForce" "" "STG"
+   add "$dir" "V" "Star Force.mra" "_StarForce" "" "STG"
    add "$dir" "V" "Vimana (Japan).mra" "_Vimana" "" "STG"
 
    add "$dir" "H" "Baluba-louk no Densetsu (Japan).mra" "_Baluba-louk no Densetsu"
