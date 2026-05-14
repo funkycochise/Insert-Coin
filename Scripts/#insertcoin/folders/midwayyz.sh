171688b26b3f868e48601d1209c608cce00577df
funkycochise <funkycochise@free.fr>
2026-05-14 15:12:42 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/midwayyz.sh
+++ Scripts/#insertcoin/folders/midwayyz.sh
@@ -13,13 +13,13 @@ dir=$midwaytyz
 resh=$(exist "Mortal Kombat (Rev. 4.0 09-28-92).mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    outdir=$1
-   add "$dir" "H"  "High Impact Football (rev LA5 02-15-91).mra" "_High Impact Football"
+   #add "$dir" "H"  "High Impact Football (rev LA5 02-15-91).mra" "_High Impact Football"
    add "$dir" "H"  "Mortal Kombat (Rev. 4.0 09-28-92).mra" "_Mortal Kombat" "" "VSF"
    add "$dir" "H"  "Mortal Kombat II (Rev. L3.1).mra" "_Mortal Kombat II" "" "VSF"
    add "$dir" "H"  "Smash T.V. (Rev. 8.00).mra" "_Smash TV"
    add "$dir" "H"  "NARC (Rev. 7.00).mra" "_NARC"
-   add "$dir" "H"  "Super High Impact (rev LA1 09-30-91).mra" "_Super High Impact (rev LA1 09-30-91)"
-   add "$dir" "H"  "Terminator 2 - Judgment Day (rev LA4 08-03-92).mra" "_Terminator 2 - Judgment Day"
-   add "$dir" "H"  "Total Carnage (rev LA1 03-10-92).mra" "_Total Carnage"
+   #add "$dir" "H"  "Super High Impact (rev LA1 09-30-91).mra" "_Super High Impact (rev LA1 09-30-91)"
+   #add "$dir" "H"  "Terminator 2 - Judgment Day (rev LA4 08-03-92).mra" "_Terminator 2 - Judgment Day"
+   #add "$dir" "H"  "Total Carnage (rev LA1 03-10-92).mra" "_Total Carnage"
    dot
 fi
