9e1962481ad3c7907793e4f4dde2b7562d8ac1f7
funkycochise <funkycochise@free.fr>
2026-05-23 08:38:41 +0200

    Add files via upload

---
+++ Scripts/#insertcoin/folders/nmk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ./folders/functions.sh
+loadsetup
+
+if [ -f "$names" ]; then
+   source <(grep nmk $names)
+else
+   nmk="_NMk"
+fi
+dir=$nmk
+
+resv=$(exist "Argus.mra")
+resh=$(exist "US AAF Mustang (25th May. 1990).mra")
+if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
+   outdir=$1
+
+   add "$dir" "V" "Argus.mra" "_Argus" "" "STG"
+
+   add "$dir" "H" "US AAF Mustang (25th May. 1990).mra" "_US AAF Mustang" "" "STG"
+   dot
+fi
