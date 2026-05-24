e5c45044bf1ec3615af499229fe5440ff573c6fe
funkycochise <funkycochise@free.fr>
2026-05-24 09:23:36 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/nmk.sh
+++ Scripts/#insertcoin/folders/nmk.sh
@@ -16,7 +16,7 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
 
    add "$dir" "V" "Argus.mra" "_Argus" "" "STG"
 
-   add "$dir" "V" "Super Dimension Fortress Macross.mra" "_Super Dimension Fortress Macross" "" "STG"
+   add "$dir" "V" "Super Dimension Fortress Macross (Japan).mra" "_Super Dimension Fortress Macross" "" "STG"
    add "$dir" "H" "US AAF Mustang (25th May. 1990).mra" "_US AAF Mustang" "" "STG"
  
    dot
