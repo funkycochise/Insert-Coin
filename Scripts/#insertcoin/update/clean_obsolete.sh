16a37c2339abd789871c40316db6b823da82d6d7
funkycochise <funkycochise@free.fr>
2026-06-08 07:25:33 +0200

    igspgm

--- Scripts/#insertcoin/update/clean_obsolete.sh
+++ Scripts/#insertcoin/update/clean_obsolete.sh
@@ -167,6 +167,8 @@ find $CORE -maxdepth 1 -type f -name "StarWars_*" ! -name "StarWars_20260603.rbf
 
 find $CORE -maxdepth 1 -type f -name "Tempest_*" ! -name "Tempest_20260607.rbf" -delete
 
+find $CORE -maxdepth 1 -type f -name "IGSPGM_*" ! -name "IGSPGM_20260608.rbf" -delete
+
 
 if [ -f "$CORE/mazinger_mister_20250804.rbf" ] 
 then
