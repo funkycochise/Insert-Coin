83c93e901f27dd4e874a6c3b3abff4bef0859136
funkycochise <funkycochise@free.fr>
2026-06-11 08:47:07 +0200

    Update suppr_#insertcoin.sh

--- Scripts/#insertcoin_maint/suppr_#insertcoin.sh
+++ Scripts/#insertcoin_maint/suppr_#insertcoin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /media/fat/Scripts/#insertcoin/folders/setup.sh
 echo "   Suppress Scripts/#insertcoin folder"
-rm -r /media/Scripts/#insertcoin
+rm -r /media/fat/Scripts/#insertcoin
 echo -e "   ${GREEN}${CHECK}${NC} Completed"
