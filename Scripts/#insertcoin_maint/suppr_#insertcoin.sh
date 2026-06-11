27c32c326c5c2fccc86aadf2a8a41c5f2515d5de
funkycochise <funkycochise@free.fr>
2026-06-11 08:16:58 +0200

    maintenance scripts

---
+++ Scripts/#insertcoin_maint/suppr_#insertcoin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /media/fat/Scripts/#insertcoin/folders/setup.sh
+echo "   Suppress Scripts/#insertcoin folder"
+rm -r /media/Scripts/#insertcoin
+echo -e "   ${GREEN}${CHECK}${NC} Completed"
---
+++ Scripts/#insertcoin_maint/suppr_arcade.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /media/fat/Scripts/#insertcoin/folders/setup.sh
+
+echo "   Suppress _Arcade folder"
+rm -r /media/fat/_Arcade
+echo -e "   ${GREEN}${CHECK}${NC} Completed"
+
