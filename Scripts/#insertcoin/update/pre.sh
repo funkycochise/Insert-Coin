bb3351f25df47457e0a9c9984e61cb2512226017
funkycochise <funkycochise@free.fr>
2026-06-13 08:21:01 +0200

    suppr macross

--- Scripts/#insertcoin/update/pre.sh
+++ Scripts/#insertcoin/update/pre.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function macross {
+COINOPKEY="/media/fat/games/mame/coinopkey.zip"
+
+if [ ! -f $COINOPKEY ]; then
+   find /media/fat/_Arcade/ -maxdepth 1 -type f -name "Super Dimension Fortress Macross*" -delete
+   find /media/fat/_Arcade/cores -maxdepth 1 -type f -name "macross_mister*" -delete
+fi
+}
+
 file="/media/fat/_Arcade/Cabal PGM.mra"
 if [ -f "$file" ]; then
    rm -r "$file"
@@ -9,3 +18,5 @@ if [ -d "$dir" ]
 then
    rm -r "$dir"
 fi
+
+macross
