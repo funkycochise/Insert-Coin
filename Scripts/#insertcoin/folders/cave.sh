ce38365e3d7d941b35fb0182ffef2b3dbdd03c4f
funkycochise <funkycochise@free.fr>
2026-06-15 16:03:48 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/cave.sh
+++ Scripts/#insertcoin/folders/cave.sh
@@ -2,23 +2,18 @@
 source ./folders/functions.sh
 dir=$cave
 
-resh=$(exist "Puzzle Uo Poko.mra")
-resv=$(exist "DoDonPachi.mra")
+resh=$(exist "Puzzle Uo Poko (World).mra")
+resv=$(exist "DoDonPachi (World, 1997 25 Master Ver.).mra")
 if  [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
-   add "$dir" "V" "Akuu Gallet (Japan).mra" "_Air Gallet" "" "STG"
-   add "$dir" "V" "Dangun Feveron.mra" "_Dangun Feveron" "" "STG"
-   add "$dir" "V" "Fever SOS.mra" "_Dangun Feveron" "" "STG"
-   add "$dir" "V" "DoDonPachi.mra" "_DoDonPachi" "" "STG"
-   add "$dir" "V" "DonPachi.mra" "_DonPachi" "" "STG"
-   add "$dir" "V" "ESP Ra.De..mra" "_ESP Ra.De" "" "STG"
-   add "$dir" "V" "Guwange.mra" "_Guwange" "" "STG"
-   add "$dir" "V" "Hotdog Storm.mra" "_Hotdog Storm" "" "STG"
-   add "$dir" "V" "Mazinger Z (Japan).mra" "_Mazinger Z" "" "STG"
+   add "$dir" "V" "DoDonPachi (World, 1997 25 Master Ver.).mra" "_DoDonPachi" "" "STG"
+   add "$dir" "V" "DonPachi (US).mra" "_DonPachi" "" "STG"
+   add "$dir" "V" "ESP Ra.De. (World 1998 422 International Ver).mra" "_ESP Ra.De" "" "STG"
+   add "$dir" "V" "Fever SOS (World, 980925 VER).mra" "_Fever SOS" "" "STG"
+   add "$dir" "V" "Guwange (Japan, 1999 624 Master Ver 1655).mra" "_Guwange" "" "STG"
 
    add "$dir" "H" "Gaia Crusaders.mra" "_Gaia Crusaders"
-   add "$dir" "H" "Puzzle Uo Poko.mra" "_Puzzle Uo Poko" "" "PUZ"
-   add "$dir" "H" "Thunder Heroes.mra" "_Thunder Heroes"
-   add "$dir" "H" "Bishoujo Senshi Sailor Moon (Version 95-03-22B, Japan).mra" "_Pretty Soldier Sailor Moon" "" "BEA"
+   add "$dir" "H" "Puzzle Uo Poko (World).mra" "_Puzzle Uo Poko" "" "PUZ"
+   add "$dir" "H" "Thunder Heroes (set 1).mra" "_Thunder Heroes"
 
    dot
 fi
