b26c701853284b6231132cb2924ea4bae3a81b03
funkycochise <funkycochise@free.fr>
2026-06-26 10:39:46 +0200

    Add files via upload

--- Scripts/#insertcoin/update/update_main_mister.sh
+++ Scripts/#insertcoin/update/update_main_mister.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
-source /media/fat/Scripts/#insertcoin/folders/functions.sh
-
-loadsetup
-
-cd /media/fat
-MF=MiSTer
+source /media/fat/Scripts/#insertcoin/folders/setup.sh
 
 echo -e "Installing custom Main"
 
+#delete backup if it already exists
 if [ -f "/media/fat/Mister_" ]; then
-   rm /media/fat/Mister_
+   echo "remove backup"
+   #rm /media/fat/Mister_
 fi
-#echo "Renaming current main mister."
-cp /media/fat/Mister /media/fat/Mister_
-rm /media/fat/Mister
 
-#echo "Downloading latest main mister."
-curl https://raw.githubusercontent.com/funkycochise/Main_MiSTer/master/releases/$MF /media/fat -O -k -s
-if test -f "/media/fat/Mister"; then
-   echo -e "${GREEN}${CHECK}${NC} Completed"
-   rm /media/fat/Mister_
-else
-   echo -e "Something went wrong while trying to download main Mister."
-   echo -e "Previous Main Mister is restored."
-   cp /media/fat/Mister_ /media/fat/Mister
+if [ -f "/media/fat/Mister_" ] && [ -f "/media/fat/Mister" ]; then
    rm /media/fat/Mister_
 fi
 
-echo ""
-
+#echo -e "getting custom Main"
+wget -q -c -P /media/fat/Scripts/temp https://github.com/funkycochise/Main_MiSTer/tree/master/releases/MiSTer
+if [ -f "/media/fat/Scripts/temp/Mister" ]; then
+   #echo "/media/fat/Scripts/temp/Mister downloaded, installing"
+   cp /media/fat/Scripts/temp/Mister /media/fat/Mister
+fi
+echo -e "${GREEN}${CHECK}${NC} Completed"
