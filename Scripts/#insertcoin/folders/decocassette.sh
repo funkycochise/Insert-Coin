7874e458be81fd6b21da322a721597880298fcca
funkycochise <funkycochise@free.fr>
2026-07-01 10:23:47 +0200

    Add files via upload

---
+++ Scripts/#insertcoin/folders/decocassette.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+source ./folders/functions.sh
+if [ "$show_system" == "1" ]; then
+   dir=$decocassette
+else
+   dir=$deco
+fi
+
+resh=$(exist "Cobra-Command (World, Rev. 5).mra")
+resv=$(exist "Burger Time (Set 1).mra")
+if  [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
+
+   add "$dir" "V" "18 Challenge Pro Golf (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Angler Dangler (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Astro Fantasia (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Boulder Dash (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Bump 'n' Jump (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Burger Time (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Burnin Rubber (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Burnin Rubber (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Burnin Rubber (set 2) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Cluster Buster (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "DECO Darksoft Multigame (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "DECO Test Tape (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Disco No.1 (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "DS Telejan (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Fighting Ice Hockey (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Fishing (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Flash Boy (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Flying Ball (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Graplop (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Graplop (Prototype) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Hamburger (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Highway Chase (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "La-Pa-Pa (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Lock'n'Chase (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Lock'n'Chase (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Lucky Poker (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Manhattan (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Mission-X (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Nebula (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Night Star (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Night Star (set 2) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Ocean to Ocean (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Ocean to Ocean (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Oozumou - Grand Sumo (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Peter Pepper's Ice Cream Factory (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Peter Pepper's Ice Cream Factory (set 2) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Pro Bowling (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Pro Golf (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Pro Golf (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Pro Soccer (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Pro Soccer (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Pro Tennis (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Pro Tennis (Japan) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Rootin' Tootin' (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Scrum Try (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Scrum Try (set 2) (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Skater (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Super Astro Fighter (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Super Doubles Tennis (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Sweet Heart (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Terranean (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "The Tower (DECO).mra" "_Deco-Cassette" "" ""
+   add "$dir" "V" "Tornado (DECO).mra" "_Deco-Cassette" "" ""
+   dot
+fi
--- Scripts/#insertcoin/folders/gaelco.sh
+++ Scripts/#insertcoin/folders/gaelco.sh
@@ -1,11 +1,16 @@
 #!/bin/bash  
 source ./folders/functions.sh
 
-   dir=$gaelco
+dir=$gaelco
 
 resh=$(exist "World Rally Championship (set 2).mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then  
 
+   add "$dir" "H" "Alligator Hunt.mra" "_Alligator Hunt" "" "ACT"
+   add "$dir" "H" "Biomechanical Toy.mra" "_Biomechanical Toy" "" "ACT"
+   add "$dir" "H" "Squash.mra" "_Squash" "" "SPO"
+   add "$dir" "H" "TH Strikes Back.mra" "_TH Strikes Back"
+   add "$dir" "H" "Thunder Hoop.mra" "_Thunder Hoop"
    add "$dir" "H" "World Rally Championship (set 2).mra" "_World Rally Championship" "" "SPO"
 
   dot
