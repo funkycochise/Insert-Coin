1d0544ddb4afe206a7ded92b2a07b4f282628c28
funkycochise <funkycochise@free.fr>
2026-07-03 06:40:58 +0200

    Update clean_obsolete.sh

--- Scripts/#insertcoin/update/clean_obsolete.sh
+++ Scripts/#insertcoin/update/clean_obsolete.sh
@@ -165,7 +165,6 @@ find $CORE -maxdepth 1 -type f -name "StarWars_*" ! -name "StarWars_20260603.rbf
 
 find $CORE -maxdepth 1 -type f -name "Tempest_*" ! -name "Tempest_20260607.rbf" -delete
 
-find $CORE -maxdepth 1 -type f -name "IGSPGM_*" ! -name "IGSPGM_20260615.rbf" -delete
 
 find $CORE -maxdepth 1 -type f -name "PsikyoSH2_*" ! -name "PsikyoSH2_20260628.rbf" -delete
 
