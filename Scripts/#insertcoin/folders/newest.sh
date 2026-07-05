ad4982da613712c024ea1a6cca0808e76c8ba948
funkycochise <funkycochise@free.fr>
2026-07-05 10:19:48 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/newest.sh
+++ Scripts/#insertcoin/folders/newest.sh
@@ -66,7 +66,7 @@ function additem {
 counter=0
 
 #0705
-   add "Boogie Wings (Euro v1.5, 92.12.07).mra" "_"Boogie Wings" 
+   add "Boogie Wings (Euro v1.5, 92.12.07).mra" "_Boogie Wings" 
    add "Break Thru (World).mra" "_Break Thru"
    add "Paperboy (rev 3).mra" "_Paperboy"
    add "Klax.mra" "_Klax"
