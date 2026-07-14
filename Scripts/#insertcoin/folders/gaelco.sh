772598e07686e548d6f8623b254067ef57676347
funkycochise <funkycochise@free.fr>
2026-07-14 10:19:17 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/gaelco.sh
+++ Scripts/#insertcoin/folders/gaelco.sh
@@ -8,6 +8,7 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
 
    add "$dir" "H" "Alligator Hunt.mra" "_Alligator Hunt" "" "ACT"
    add "$dir" "H" "Biomechanical Toy.mra" "_Biomechanical Toy" "" "ACT"
+   add "$dir" "H" "Glass.mra" "_Glass" "" ""
    add "$dir" "H" "Squash.mra" "_Squash" "" "SPO"
    add "$dir" "H" "TH Strikes Back.mra" "_TH Strikes Back"
    add "$dir" "H" "Thunder Hoop.mra" "_Thunder Hoop"
