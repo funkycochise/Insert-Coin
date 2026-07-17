e5ba06c8b29639bc7e6e8e2273a399ad0b133987
funkycochise <funkycochise@free.fr>
2026-07-17 14:13:22 +0200

    Add files via upload

---
+++ Scripts/#insertcoin/folders/kaneko.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+source ./folders/functions.sh
+
+dir=$kaneko
+
+resh=$(exist "Gals Panic (unprotected, ver. 2.0).mra")
+if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then  
+
+   add "$dir" "H" "Gals Panic (unprotected, ver. 2.0).mra" "_Gals Panic" "" "PUZ"
+
+
+  dot
+fi
