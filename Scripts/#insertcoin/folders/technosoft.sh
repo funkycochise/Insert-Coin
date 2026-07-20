d1bc366e8956df81838466ccc58c9723001600f7
funkycochise <funkycochise@free.fr>
2026-07-20 13:34:45 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/technosoft.sh
+++ Scripts/#insertcoin/folders/technosoft.sh
@@ -6,6 +6,7 @@ resh=$(exist "Hyper Duel.mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
 
    add "$dir" "H" "Hyper Duel.mra" "_Hyper Duel" "" "STG"
+   add "$dir" "H" "Magical Error wo Sagase.mra" "_Magical Error wo Sagase" "" "PUZ"
 
    dot
 fi
