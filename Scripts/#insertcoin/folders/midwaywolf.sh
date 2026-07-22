25f3cf52faea9a218985527418ef5308e920b2ce
funkycochise <funkycochise@free.fr>
2026-07-22 17:16:07 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/midwaywolf.sh
+++ Scripts/#insertcoin/folders/midwaywolf.sh
@@ -6,6 +6,7 @@ resh=$(exist "NBAHangtime.mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "H"  "NBAHangtime.mra" "_NBAHangtime" "" "SPO"
    add "$dir" "H"  "Ultimate Mortal Kombat 3.mra" "_Ultimate Mortal Kombat 3" "" "VSF"
+   add "$dir" "H"  "WWFWrestleMania.mra" "_WWFWrestleMania" "" "VSF"
 
    dot
 fi
