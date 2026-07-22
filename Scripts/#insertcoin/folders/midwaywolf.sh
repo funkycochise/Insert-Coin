0c2e92defd30ff3b37da15affe75a0546b0270c7
funkycochise <funkycochise@free.fr>
2026-07-22 16:28:17 +0200

    Update midwaywolf.sh

--- Scripts/#insertcoin/folders/midwaywolf.sh
+++ Scripts/#insertcoin/folders/midwaywolf.sh
@@ -5,6 +5,7 @@ dir=$midwaywolf
 resh=$(exist "NBAHangtime.mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "H"  "NBAHangtime.mra" "_NBAHangtime" "" "SPO"
+   add "$dir" "H"  "Ultimate Mortal Kombat 3.mra" "_Ultimate Mortal Kombat 3" "" "VSF"
 
    dot
 fi
