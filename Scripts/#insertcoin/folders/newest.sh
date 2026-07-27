0c6a094c868a9a5517db8a4f17f959471647e92e
funkycochise <funkycochise@free.fr>
2026-07-27 10:33:28 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/newest.sh
+++ Scripts/#insertcoin/folders/newest.sh
@@ -66,6 +66,7 @@ function additem {
 counter=0
 
 #0727
+   add "Holosseum (US, Rev A).mra" "_Holosseum"
    add "Golden Axe The Revenge of Death Adder (World, Rev B).mra" "_Golden Axe The Revenge of Death Adder"
    add "TrackPak.mra" "_SuperOffRoad"
 
