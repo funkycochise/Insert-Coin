11a4f97a13eb3c3d95069122a3a44b5318d5d8f3
funkycochise <funkycochise@free.fr>
2026-07-28 19:59:17 +0200

    Add files via upload

--- Scripts/#insertcoin/update/res.sh
+++ Scripts/#insertcoin/update/res.sh
@@ -160,6 +160,7 @@ dl "blahm1d_Rampage.zip" "Rampage"
 dl "blahm1d_lkage.zip" "The legend of Kage"
 #dl "blahm1d_NBAHangtime.zip" "NBA HAngtime"
 dl "WolfUnit.zip" "Midway Wolf"
+dl "Raiden.zip"
 
 #force goldenaxe2 core
 find /media/fat/_Arcade/cores -maxdepth 1 -type f -name "s32GoldenAxe.rbf" -delete
