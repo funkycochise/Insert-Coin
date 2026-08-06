ae173b8067cd1dc758bbff41fe25119cffa3f42b
funkycochise <funkycochise@free.fr>
2026-08-06 10:57:49 +0200

    Add files via upload

--- Scripts/#insertcoin/update/clean_obsolete.sh
+++ Scripts/#insertcoin/update/clean_obsolete.sh
@@ -138,13 +138,9 @@ then
 fi
 #tar Force
 find $CORE -maxdepth 1 -type f -name "StarForce_*" ! -name "Starforce_20260418.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "NaughtyBoy_*" ! -name "NaughtyBoy_20250428.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Darius_*" ! -name "Darius_20260502.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "ChinaGate*" ! -name "ChinaGate_20260506.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "jtargus_*" ! -name "jtargus_20260502.rbf" -delete
 
 
@@ -158,42 +154,27 @@ if [ -f "$CORE/Darius2_20260430.rbf" ] && [ -f "$CORE/Darius2_20260510.rbf" ]; t
    find /media/fat/_Arcade/cores  -maxdepth 1 -type f -name "Darius2_*" ! -name "Darius2_20260510.rbf" -delete
 fi
 find $CORE -maxdepth 1 -type f -name "Millipede_*" ! -name "Millipede_20260518.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "snowbros_mister_*" ! -name "snowbros_mister_20260527.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Tempest_*" ! -name "Tempest_20260720.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "PsikyoSH2_*" ! -name "PsikyoSH2_20260731.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "jtnslasher_*" ! -name "jtnslasher_20260707.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "HeatedBarrel_*" ! -name "HeatedBarrel_20260715.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Legionnaire_*" ! -name "Legionnaire_20260715.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "MajorHavoc_*" ! -name "MajorHavoc_20260730.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "CloakAndDagger_*" ! -name "CloakAndDagger_20260708.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "XNSYSTEM11_*" ! -name "XNSYSTEM11_20260727.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "XNZN1_*" ! -name "XNZN1_20260721.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Paperboy_*" ! -name "Paperboy_20260730.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Klax_*" ! -name "Klax_20260729.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Toobin_*" ! -name "Toobin_20260729.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "glass_*" ! -name "glass_20260713.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Asteroids_*" ! -name "Asteroids_20260804.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "Batsugun_*" ! -name "Batsugun_20260726.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "SuperOffRoad_*" ! -name "SuperOffRoad_20260726.rbf" -delete
-
 find $CORE -maxdepth 1 -type f -name "NARC_*" ! -name "NARC_20260730.rbf" -delete
+find $CORE -maxdepth 1 -type f -name "segas24_*" ! -name "segas24_20260806.rbf" -delete
+
+
 
 if [ -f "$CORE/Darius_20260411.rbf" ] 
 then
