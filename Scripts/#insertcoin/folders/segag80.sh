853f105b2f749758cfb17efeb49e2cbbe5628fbe
funkycochise <funkycochise@free.fr>
2026-08-15 15:31:28 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/segag80.sh
+++ Scripts/#insertcoin/folders/segag80.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bash  
 source ./folders/functions.sh
 if [ "$show_system" == "1" ]; then
    dir=$segag80
 else
    dir=$sega
 fi
-#echo "$dir"
-resh=$(exist "005.mra")
-resv=$(exist "Astro Blaster.mra")
+
+resh=$(exist "Star Trek.mra")
+resv=""
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "005.mra" "_005" "" "STG"
    add "$dir" "V" "Astro Blaster.mra" "_Astro Blaster" "" "STG"
@@ -15,5 +15,7 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "Pig Newton.mra" "_Pig Newton" "" "ACT"
    add "$dir" "V" "Sindbad Mystery.mra" "_Sindbad Mystery" "" "ACT"
    add "$dir" "V" "Space Odyssey.mra" "_Space Odyssey" "" "STG"
+   add "$dir" "H" "Star Trek.mra" "_Star Trek" "" ""
+   add "$dir" "H" "Tac-Scan.mra" "_Tac-Scan" "" "STG"
    dot
 fi
