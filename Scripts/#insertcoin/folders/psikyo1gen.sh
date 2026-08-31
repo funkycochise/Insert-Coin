d6f78b7261bb72978053e3032573bb395226aff8
funkycochise <funkycochise@free.fr>
2026-08-31 11:56:28 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/psikyo1gen.sh
+++ Scripts/#insertcoin/folders/psikyo1gen.sh
@@ -7,11 +7,14 @@ else
 fi
 
 resv=$(exist "Gunbird (World).mra")
+resh=$(exist "Tengai (World).mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
 
    add "$dir" "V"  "Battle K-Road.mra" "_Battle K-Road" "" "VSF"
    add "$dir" "V"  "Gunbird (World).mra" "_Gunbird" "" "STG"
-   add "$dir" "H"  "Samurai Aces (World).mra" "_Samurai Aces" "" "STG"
+   add "$dir" "V"  "Samurai Aces (World).mra" "_Samurai Aces" "" "STG"
+   add "$dir" "V"  "Strikers 1945 (World).mra" "_Strikers 1945" "" "STG"
+   add "$dir" "H"  "Tengai (World).mra" "_Tengai" "" "STG"
 
    dot
 fi
