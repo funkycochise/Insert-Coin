5c2bea64132d6f04befb1231bddcb65a6683cd7b
funkycochise <funkycochise@free.fr>
2026-09-05 20:14:16 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/sega.sh
+++ Scripts/#insertcoin/folders/sega.sh
@@ -12,8 +12,11 @@ if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "Super Zaxxon (315-5013).mra" "_Super Zaxxon" "" "ACT"
    add "$dir" "V" "Zaxxon (Set 1, Rev D).mra" "_Zaxxon" "" "ACT"
 
-   add "$dir" "H" "Buck Rogers Planet of Zoom (not encrypted, set 1).mra" "_Buck Rogers Planet of Zoom" "" "STG"
    add "$dir" "H" "BankPanic.mra" "_BankPanic"
+   add "$dir" "H" "Buck Rogers Planet of Zoom (not encrypted, set 1).mra" "_Buck Rogers Planet of Zoom" "" "STG"
+   add "$dir" "H" "Hang-On.mra" "_Hang-On" "" "RAC"
+   add "$dir" "H" "Space Harrier (Rev A, 8751 315-5163A).mra" "_Space Harrier" "" "ACT"
    add "$dir" "H" "Super Locomotive.mra" "_Super Locomotive" "" "ACT"
+
    dot
 fi
