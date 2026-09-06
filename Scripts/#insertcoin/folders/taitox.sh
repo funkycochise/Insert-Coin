7f907e024aeb134c34681f183a20ed7efec4b797
funkycochise <funkycochise@free.fr>
2026-09-06 06:20:16 +0200

    Add files via upload

--- Scripts/#insertcoin/folders/taitox.sh
+++ Scripts/#insertcoin/folders/taitox.sh
@@ -10,6 +10,9 @@ resh=""
 resv=$(exist "Twin Hawk (World).mra")
 if [ "$resh" == "1" ] || [ "$resv" == "1" ]; then
    add "$dir" "V" "Twin Hawk (World).mra" "_Twin Hawk" "" "STG"
-
+   add "$dir" "H" "Superman (World).mra" "_Superman" "" "ACT"
+   add "$dir" "H" "Gigandes.mra" "_Gigandes" "" "STG" 
+   add "$dir" "H" "Balloon Brothers.mra" "_Balloon Brothers" "" "PUZ"
+   add "$dir" "H" "Last Striker - Kyuukyoku no Striker.mra" "_Last Striker - Kyuukyoku no Striker" "" "SPO"
   dot
 fi
